a261d573541554bc41eb6f63e8cbfefe09db6562
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-01-10 21:06:16 -0600

    Update test file

--- test.php
+++ test.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 require 'src/bootstrap.php';
 
+use PhpAndMysqlBook\Validate\Validate;
+
 $user_role = 'admin';
 
 is_admin($user_role);
@@ -17,3 +19,11 @@ if ($exception === true) {
 $some_array = ['a', 'b', 'c'];
 //echo $some_array[3];
 
+echo 'Correct number: ';
+var_dump(Validate::isNumber(100));
+echo '<br>';
+echo 'Incorrect number: ';
+var_dump(Validate::isNumber(9999));
+echo '<br>';
+echo 'Correct password: ';
+var_dump(Validate::isPassword('Lobos1234'));
