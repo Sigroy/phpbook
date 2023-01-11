4d5def0d91fb6e6a66dd659615c2ff8393de321b
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-01-11 17:19:48 -0600

    Create register script and html

--- src/pages/register.php
+++ src/pages/register.php
@@ -18,8 +18,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $errors['forname'] = Validate::isText($member['forename'], 1, 254)
         ? '' : 'Forename must be 1-254 characters';
+    $errors['surname'] = Validate::isText($member['surname'], 1, 254)
+        ? '' : 'Surname must be 1-254 characters';
+    $errors['email'] = Validate::isEmail($member['email']) ? '' : 'Please enter a valid email';
+    $errors['password'] = Validate::isPassword($member['password'])
+        ? '' : 'Passwords must be at least 8 characters and have:<br>
+               A lowercase letter<br>An uppercase letter<br>A number<br>
+                And a special character';
+    $errors['confirm'] = ($member['password'] === $confirm)
+        ? '' : 'Passwords do not match';
+
+    $invalid = implode($errors);
+
+    if (!$invalid) {
+        $result = $cms->getMember()->create($member);
+        if ($result === false) {
+            $errors['email'] = 'Email address already used';
+        } else {
+            redirect('login.php', ['success' => 'Thanks for joining! Please log in.']);
+        }
+    }
 }
 
+$data['navigation'] = $cms->getCategory()->getAll();
 $data['member'] = $member;
 $data['errors'] = $errors;
 
